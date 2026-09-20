948caecdcc90b7796197f078105228eb23a397b8
Deep-Bera <144211573+Deep-Bera@users.noreply.github.com>
2026-09-20 10:37:50 +0530

    changed the connection process in DB.js for lower connection time ....

--- Backend/app/config/db.js
+++ Backend/app/config/db.js
@@ -1,10 +1,17 @@
 import mongoose from "mongoose";
+
+let cachedConnection = null;
+
 const configDB = async () => {
+  if (cachedConnection) return cachedConnection; // if already connected, reuse it..
+
   try {
-    const db = await mongoose.connect(process.env.MONGODB_URL);
+    cachedConnection = await mongoose.connect(process.env.MONGODB_URL);
     console.log("Server connected to DB project_db_final");
+    return cachedConnection;
   } catch (error) {
     console.log("Connection failed for ", error.message);
   }
 };
+
 export default configDB;
